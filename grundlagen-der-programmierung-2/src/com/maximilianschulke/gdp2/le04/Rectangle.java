eca471407187ceef1029ef57183955a8c4f9cdc0
Mara Schulke <mara@schulke.xyz>
2020-09-03 23:00:16 +0200

    cleanup the rectangle class

--- grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le04/Rectangle.java
+++ grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le04/Rectangle.java
@@ -3,42 +3,72 @@ package com.maximilianschulke.gdp2.le04;
 
 public class Rectangle {
 
+	private double x;
+	private double y;
 	private double a;
 	private double b;
 
+	
 	public Rectangle(double x, double y, double lengthA, double lengthB) {
-		a = lengthA;
-		b = lengthB;
+
 	}
 
+	
 	public double area() {
 		return a * b;
 	}
 
-	public double circumference() {
+	
+	public double perimeter() {
 		return a + a + b + b;
 	}
+
+	
+	public void show() {
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("area = " + area());
+		System.out.println("perimeter = " + perimeter());
+	}
+
+
+	public double getX() {
+		return x;
+	}
+
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+
+	public double getY() {
+		return y;
+	}
+
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
 
 	public double getA() {
 		return a;
 	}
 
-	public void setA(double newA) {
-		a = newA;
+
+	public void setA(double a) {
+		this.a = a;
 	}
+
 
 	public double getB() {
 		return b;
 	}
 
-	public void setB(double newB) {
-		b = newB;
+
+	public void setB(double b) {
+		this.b = b;
 	}
 
-	public void show() {
-		System.out.println("a = " + a);
-		System.out.println("b = " + b);
-		System.out.println("area = " + area());
-		System.out.println("circumference = " + circumference());
-	}
 }
