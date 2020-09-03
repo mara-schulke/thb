e573ccf7bdb47cecee7e1daaeb5b6e8985adf9b4
Mara Schulke <mara@schulke.xyz>
2020-09-03 23:00:50 +0200

    add more examples

--- grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le04/Test.java
+++ grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le04/Test.java
@@ -7,6 +7,10 @@ public class Test {
 		Ellipse e = new Ellipse(0, 0, 10, 10);
 		System.out.println("Eclipse:");
 		e.show();
+		
+		Circle c = new Circle(0, 0, 15);
+		System.out.println("\nCircle:");
+		c.show();
 	}
 
 }
