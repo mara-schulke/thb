e27a537a10746528078b3c5d149240f9ca3547fa
Mara Schulke <mara@schulke.xyz>
2020-09-03 22:52:14 +0200

    add a circle which extends the ellipse

---
+++ grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le04/Circle.java
@@ -0,0 +1,25 @@
+package com.maximilianschulke.gdp2.le04;
+
+public class Circle extends Ellipse {	
+	
+	public Circle(double x, double y, double radius) {
+		super(x, y, radius, radius);
+	}
+
+	
+	public double circumference() {
+		return perimeter();
+	}
+	
+
+	public double getRadius() {
+		return getRadiusX();
+	}
+
+
+	public void setRadius(double radius) {
+		setRadiusX(radius);
+		setRadiusY(radius);
+	}
+	
+}
