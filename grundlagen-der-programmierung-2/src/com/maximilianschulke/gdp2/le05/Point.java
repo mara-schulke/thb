efb832b1cc55167d9189206f6637cbb36cc467b5
Mara Schulke <mara@schulke.xyz>
2020-09-07 22:38:46 +0200

    add a point class to unify positioning

---
+++ grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le05/Point.java
@@ -0,0 +1,49 @@
+package com.maximilianschulke.gdp2.le05;
+
+public class Point {
+
+	private double x;
+	private double y;
+
+
+	public Point (double x, double y) {
+		this.setX(x);
+		this.setY(y);
+	}
+
+
+	public double xDistanceTo(Point p) {
+		return p.getX() - this.getX();
+	}
+
+
+	public double yDistanceTo(Point p) {
+		return p.getY() - this.getY();
+	}
+
+
+	public double getX() {
+		return x;
+	}
+
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+
+	public double getY() {
+		return y;
+	}
+
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+
+	public String toString() {
+		return "[" + getX() + "; " + getY() + "]";
+	}
+
+}
