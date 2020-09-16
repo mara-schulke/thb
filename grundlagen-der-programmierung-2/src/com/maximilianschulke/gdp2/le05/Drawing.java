fcdc191fc0ebd56e5ff20000ec77194a9a947da3
Mara Schulke <mara@schulke.xyz>
2020-09-16 16:53:48 +0200

    add a drawing class

---
+++ grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le05/Drawing.java
@@ -0,0 +1,38 @@
+package com.maximilianschulke.gdp2.le05;
+
+import java.util.ArrayList;
+
+public class Drawing {
+
+	private ArrayList<Figure> figures;
+
+
+	public Drawing(Figure[] figs) {
+		this.figures = new ArrayList<Figure>();
+
+		for (Figure f : figs) {
+			add(f);
+		}
+	}
+
+
+	public void add(Figure f) {
+		figures.add(f);
+	}
+
+
+	public void remove(Figure f) {
+		figures.remove(f);
+	}
+
+
+	public void move(int index, Point pos) {
+		figures.get(index).setPos(pos);
+	}
+
+
+	public void clear() {
+		figures.clear();
+	}
+
+}
