7440e20623722bcdcf69f1e6861feadc8659c1a4
Mara Schulke <mara@schulke.xyz>
2020-09-16 17:21:20 +0200

    rewrite the le04 line

--- grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le04/Line.java
+++ grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le04/Line.java
@@ -2,22 +2,74 @@ package com.maximilianschulke.gdp2.le04;
 
 public class Line {
 
-	private double a;
-	private double b;
+	private double ax;
+	private double ay;
+	private double bx;
+	private double by;
 
-	public Line(double x, double y, double pointA, double pointB) {
-		a = pointA;
-		b = pointB;
+
+	public Line(double ax, double ay, double bx, double by) {
+		setAX(ax);
+		setAY(ay);
+		setBX(bx);
+		setBY(by);
 	}
+
 
 	public double length() {
-		return Math.abs(b - a);
+		double x = bx - ax;
+		double y = by - ay;
+
+		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 	}
 
-	public void show() {
-		System.out.println("a = " + a);
-		System.out.println("b = " + b);
-		System.out.println("length = " + length());
+
+	public String toString() {
+		String s = "";
+		s += "a = (" + getAX() + ";" + ay + ")\n";
+		s += "b = (" + bx + ";" + by + ")\n";
+		s += "length = " + length() + "\n";
+		return s;
+	}
+
+
+	public double getAX() {
+		return ax;
+	}
+
+
+	public void setAX(double ax) {
+		this.ax = ax;
+	}
+
+
+	public double getAY() {
+		return ay;
+	}
+
+
+	public void setAY(double ay) {
+		this.ay = ay;
+	}	
+
+
+	public double getBX() {
+		return bx;
+	}
+
+
+	public void setBX(double bx) {
+		this.bx = bx;
+	}
+
+	
+	public double getBY() {
+		return by;
+	}
+
+
+	public void setBY(double by) {
+		this.by = by;
 	}
 
 }
