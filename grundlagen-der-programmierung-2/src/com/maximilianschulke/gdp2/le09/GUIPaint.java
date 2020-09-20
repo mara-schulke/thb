6652246a4d845238f29171f8b50ffc94675e3c32
Mara Schulke <mara@schulke.xyz>
2020-09-20 08:16:59 +0200

    draw the house for le09

--- grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le09/GUIPaint.java
+++ grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le09/GUIPaint.java
@@ -4,12 +4,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.geometry.*;
-import javafx.scene.control.*;
 import javafx.scene.shape.*;
-import javafx.scene.text.*;
 
 public class GUIPaint extends Application {
 
@@ -23,39 +19,60 @@ public class GUIPaint extends Application {
 
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("GUIPaint");
+			primaryStage.setMinWidth(600);
+			primaryStage.setMinHeight(400);
 			primaryStage.show();
 
-			Rectangle r = new Rectangle(250, 100, 150, 100);
-			r.setFill(Color.GREEN);
-			r.setStroke(Color.AQUAMARINE);
+			Rectangle sky = new Rectangle(0, 000, 600, 300);
+			sky.setFill(Color.LIGHTBLUE);
 
-			Ellipse e = new Ellipse(200, 100, 100, 50);
-			e.setFill(Color.CHOCOLATE);
-			e.setStroke(Color.BROWN);
+			Rectangle earth = new Rectangle(0, 300, 600, 100);
+			earth.setFill(Color.GREEN);
 
-			Text t = new Text(130, 100, "Meine erste Zeichnung");
-			t.setFont(new Font(15));
-			t.setFill(Color.YELLOW);
+			Group house = new Group();
 
-			Shape su = Shape.union(r, e);
-			Shape ss = Shape.subtract(e,r);
-			root.getChildren().addAll(r,e,t);
+			Polygon roof = new Polygon(200.0, 150.0, 300.0, 100.0, 400.0, 150.0);
+			roof.setFill(Color.ORANGE);
 
-			scene.setOnKeyTyped(me ->{
-				System.out.println(me.getEventType()+ " " + me.getCharacter() + " " + me.getSource());
+			Rectangle body = new Rectangle(200, 150, 200, 150);
+			body.setFill(Color.BEIGE);
+
+			Rectangle window1 = new Rectangle(237.5, 175, 25, 40);
+			window1.setFill(Color.WHITE);
+
+			Rectangle window2 = new Rectangle(237.5, 225, 25, 40);
+			window2.setFill(Color.WHITE);
+
+			Rectangle door = new Rectangle(285, 250, 30, 50);
+			door.setFill(Color.SADDLEBROWN);
+
+			Rectangle window3 = new Rectangle(337.5, 175, 25, 40);
+			window3.setFill(Color.WHITE);
+
+			Rectangle window4 = new Rectangle(337.5, 225, 25, 40);
+			window4.setFill(Color.WHITE);
+
+			Rectangle window5 = new Rectangle(287.5, 175, 25, 40);
+			window5.setFill(Color.WHITE);
+
+			house.getChildren().addAll(roof, body, window1, window2, door, window3, window4, window5);
+			root.getChildren().addAll(sky, earth, house);
+
+			scene.setOnKeyTyped(me -> {
+				System.out.println(me.getEventType() + " " + me.getCharacter() + " " + me.getSource());
 			});
 
-			scene.setOnMousePressed(me ->{
+			scene.setOnMousePressed(me -> {
 				this.x0 = me.getX();
 				this.y0 = me.getY();
-				System.out.println(me.getEventType()+ " " + x0 + " "+ y0 + " "+ me.getSource());
+				System.out.println(me.getEventType() + " " + x0 + " " + y0 + " " + me.getSource());
 			});
 
-			scene.setOnMouseReleased(me->{
+			scene.setOnMouseReleased(me -> {
 				this.x1 = me.getX();
 				this.y1 = me.getY();
-				System.out.println(me.getEventType()+ " " + x1 + " "+ y1 + " "+ me.getSource());
-			root.getChildren().add(new Rectangle(x0,y0,x1-x0,y1-y0));
+				System.out.println(me.getEventType() + " " + x1 + " " + y1 + " " + me.getSource());
+				root.getChildren().add(new Rectangle(x0, y0, x1-x0, y1-y0));
 			});
 		} catch(Exception e) {
 			e.printStackTrace();
