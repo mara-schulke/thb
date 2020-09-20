df0b352d09bea134aa449598d3efd2d0fe04aac3
Mara Schulke <mara@schulke.xyz>
2020-09-20 18:20:44 +0200

    add shape positioning and mutation

--- grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le10/Controller.java
+++ grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le10/Controller.java
@@ -4,48 +4,125 @@ import com.maximilianschulke.gdp2.le10.model.*;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseDragEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.canvas.*;
+import javafx.scene.layout.Pane;
+
+
+enum FigureType {
+	CIRCLE,
+	ELLIPSE,
+	LINE,
+	RECTANGLE,
+	SQUARE,
+	POLYGON,
+	TRIANGLE;
+
+	public static FigureType fromString(String id) {
+		switch (id.toLowerCase()) {
+			case "circle":
+				return CIRCLE;
+			case "ellipse":
+				return ELLIPSE;
+			case "line":
+				return LINE;
+			case "rectangle":
+				return RECTANGLE;
+			case "square":
+				return SQUARE;
+			case "polygon":
+				return POLYGON;
+			case "triangle":
+				return TRIANGLE;
+		}
+
+		return null;
+	}
+
+
+	public Figure intoFigure(Point pos) {
+		switch (this) {
+			case CIRCLE:
+				return new Circle(pos);
+			case ELLIPSE:
+				return new Ellipse(pos);
+			case LINE:
+				return new Line(pos);
+			case RECTANGLE:
+				return new Rectangle(pos);
+			case SQUARE:
+				return new Square(pos);
+			case POLYGON:
+				return new Polygon(pos);
+			case TRIANGLE:
+				return new Triangle(pos);
+		}
+
+		return null;
+	}
+}
 
 public class Controller {
 
 	private Figure figure;
+	private Point start;
 
 	@FXML private BorderPane layout;
-	@FXML private Canvas canvas;
+	@FXML private Pane root;
 	@FXML private ChoiceBox<String> typeBox;
 
-	@FXML protected void onMouseDrag() {}
 
-	@FXML protected void onTypeSelect(ActionEvent ev) {
-		System.out.println("Selected type -> " + typeBox.getValue());
+	@FXML protected void onMouseDown(MouseEvent ev) {
+		if (!ev.isPrimaryButtonDown()) return;
 
-		switch (typeBox.getValue().toLowerCase()) {
-			case "circle":
-				figure = new Circle();
+		start = new Point(ev.getX(), ev.getY());
+		figure = FigureType.fromString(typeBox.getValue()).intoFigure(start);
+		root.getChildren().add(figure.intoShape());
+	}
+	
+
+	@FXML protected void onMouseMove(MouseEvent ev) {
+		if (!ev.isPrimaryButtonDown()) return;
+
+		Point current = new Point(ev.getX(), ev.getY());
+
+		switch (FigureType.fromString(typeBox.getValue())) {
+			case CIRCLE:
+				System.out.println("Set circle radius to " + start.xDistanceTo(current));
+				((Circle) figure).setRadius(Math.abs(start.xDistanceTo(current)));
 				break;
-			case "ellipse":
-				figure = new Ellipse();
+			case ELLIPSE:
+				((Ellipse) figure).setRadiusX(Math.abs(start.xDistanceTo(current)));
+				((Ellipse) figure).setRadiusY(Math.abs(start.yDistanceTo(current)));
 				break;
-			case "line":
-				figure = new Line();
+			case LINE:
+				((Line) figure).setDest(current);
 				break;
-			case "rectangle":
-				figure = new Rectangle();
+			case RECTANGLE:
+				((Rectangle) figure).setA(Math.abs(start.xDistanceTo(current)));
+				((Rectangle) figure).setB(Math.abs(start.yDistanceTo(current)));
 				break;
-			case "square":
-				figure = new Square();
+			case SQUARE:
+				((Square) figure).setA(Math.abs(start.xDistanceTo(current)));
+				((Square) figure).setB(Math.abs(start.xDistanceTo(current)));
 				break;
-			case "polygon":
-				figure = new Polygon();
+			case POLYGON:
 				break;
-			case "triangle":
-				figure = new Polygon();
+			case TRIANGLE:
 				break;
 		}
+	}
 
-		System.out.println(figure);
+
+	@FXML protected void onMouseUp(MouseEvent ev) {
+		if(!ev.getButton().equals(MouseButton.PRIMARY)) return;
+
+		System.out.println("End Shape");
+		start = null;
+		figure = null;
 	}
 
 }
