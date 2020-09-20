6d3d1a009a965604e44443eed3c8ad58ed4cf652
Mara Schulke <mara@schulke.xyz>
2020-09-20 08:24:40 +0200

    format le10

--- grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le10/Controller.java
+++ grundlagen-der-programmierung-2/src/com/maximilianschulke/gdp2/le10/Controller.java
@@ -9,19 +9,19 @@ import javafx.stage.Stage;
 public class Controller extends Application {
 
 	@Override
-    public void start(Stage stage) throws Exception {
-    	Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
-    
-    	Scene scene = new Scene(root, 800, 800);
-    
-    	stage.setTitle("Drawing App");
-    	stage.setScene(scene);
-    	stage.show();
-    }
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+
+		Scene scene = new Scene(root, 800, 800);
+
+		stage.setTitle("Drawing App");
+		stage.setScene(scene);
+		stage.show();
+	}
 
 
 	public static void main(String[] args) {
-	    launch(args);
+		launch(args);
 	}
 
 }
