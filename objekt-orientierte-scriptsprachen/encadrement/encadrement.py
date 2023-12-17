ef8786177561e7ebb2ff913dabd84a644d82c943
Mara Schulke <mara@schulke.xyz>
2023-12-17 10:45:13 +0100

    Restructure encadrement

--- objekt-orientierte-scriptsprachen/encadrement/encadrement.py
+++ objekt-orientierte-scriptsprachen/encadrement/encadrement.py
@@ -6,14 +6,15 @@ from tkinter.messagebox import askyesno, showerror
 
 from PIL import Image, ImageFilter, ImageGrab, ImageOps, ImageTk
 
-WIDTH = 750
-HEIGHT = 560
-STATE = State()
+WIDTH = 800
+HEIGHT = 800
 
-class State:
-    file_path = ""
+class EditorState:
     pen_size = 3
     pen_color = "black"
+
+class ImageState:
+    file_path = ""
 
 class Menubar(tk.Frame):
     def __init__(self, root=None):
@@ -28,16 +29,31 @@ class Menubar(tk.Frame):
         self.open = tk.Button(self, text='Open', command=self.quit)
         self.open.grid(row=0, column=0, columnspan=3, sticky=tk.N+tk.E+tk.S+tk.W)
 
+        # self.open_button = tk.Button(topFrame, text="Open Image", command=self.open_image)
+        # self.open_button.pack(side=tk.LEFT, padx=5, pady=5)
+        # self.resize_button = tk.Button(topFrame, text="Resize Image", command=self.resize_image)
+        # self.resize_button.pack(side=tk.LEFT, padx=5, pady=5)
+        # self.save_button = tk.Button(topFrame, text="Save Image As...", command=self.save_image_as)
+        # self.save_button.pack(side=tk.LEFT, padx=5, pady=5)
+
+        # menubar = tk.Menu(root)
+        # file_menu = tk.Menu(menubar, tearoff=0)
+        # file_menu.add_command(label="Open", command=self.open_image)
+        # file_menu.add_command(label="Save as...", command=self.save_image_as)
+        # file_menu.add_command(label="Resize", command=self.resize_image)
+        # file_menu.add_separator()
+        # file_menu.add_command(label="Exit", command=root.quit)
+        # menubar.add_cascade(label="File", menu=file_menu)
+        # root.config(menu=menubar)
+
+
 class Editor(tk.Frame):
     def __init__(self, root=None):
         tk.Frame.__init__(self, root)
         self.grid()
-        self.createWidgets()
 
-    def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit',
-            command=self.quit)
-        self.quitButton.grid()
+        self.canvas = tk.Canvas(self, bg='gray', width=800, height=800)
+        self.canvas.pack(fill=tk.BOTH, expand=True)
 
 class Layout(tk.Frame):
     def __init__(self, root):
@@ -49,80 +65,43 @@ class Layout(tk.Frame):
         self.bottom = Frame(self)
         self.bottom.pack(side=tk.BOTTOM)
 
-root = tk.Tk()
-root.title('Encadrement')
-root.geometry("800x800")
-
-layout = Layout(root)
-layout.pack()
-
-Menubar(layout.top).pack()
-Editor(layout.bottom).pack()
 
 
 # app = Application(root)
 # app.pack()
 
-root.mainloop()
 
+    # def open_image(self):
+        # self.image_path = filedialog.askopenfilename()
+        # if self.image_path:
+            # self.image = Image.open(self.image_path)
+            # self.photo = ImageTk.PhotoImage(self.image.resize((200, 200)), self.image)
+            # self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
 
-class ImageEditor:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("")
-        self.root.resizable(False, False)
+    # def save_image_as(self):
+        # if self.image:
+            # filepath = filedialog.asksaveasfilename(defaultextension=".png")
+            # if filepath:
+                # self.image.save(filepath)
 
-        self.image = None
-        self.photo = None
+    # def resize_image(self):
+        # if self.image:
+            # new_size = tk.simpledialog.askstring("Resize", "Enter new size (width,height):")
+            # if new_size:
+                # width, height = map(int, new_size.split(','))
+                # self.image = self.image.resize((width, height))
+                # self.photo = ImageTk.PhotoImage(self.image)
+                # self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+                # self.root.geometry(f"{width}x{height}")
 
-        self.image_path = ""
+if __name__ == "__main__":
+    app = tk.Tk()
+    app.title('Encadrement')
+    app.geometry(f"{WIDTH}x{HEIGHT}")
 
+    layout = Layout(app)
 
-        self.canvas = tk.Canvas(bottomFrame, bg='gray', width=800, height=800)
-        self.canvas.pack(fill=tk.BOTH, expand=True)
+    Menubar(layout.top)
+    Editor(layout.bottom)
 
-        menubar = tk.Menu(root)
-        file_menu = tk.Menu(menubar, tearoff=0)
-        file_menu.add_command(label="Open", command=self.open_image)
-        file_menu.add_command(label="Save as...", command=self.save_image_as)
-        file_menu.add_command(label="Resize", command=self.resize_image)
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=root.quit)
-        menubar.add_cascade(label="File", menu=file_menu)
-
-        self.open_button = tk.Button(topFrame, text="Open Image", command=self.open_image)
-        self.open_button.pack(side=tk.LEFT, padx=5, pady=5)
-        self.resize_button = tk.Button(topFrame, text="Resize Image", command=self.resize_image)
-        self.resize_button.pack(side=tk.LEFT, padx=5, pady=5)
-        self.save_button = tk.Button(topFrame, text="Save Image As...", command=self.save_image_as)
-        self.save_button.pack(side=tk.LEFT, padx=5, pady=5)
-
-        root.config(menu=menubar)
-
-    def open_image(self):
-        self.image_path = filedialog.askopenfilename()
-        if self.image_path:
-            self.image = Image.open(self.image_path)
-            self.photo = ImageTk.PhotoImage(self.image.resize((200, 200)), self.image)
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-
-    def save_image_as(self):
-        if self.image:
-            filepath = filedialog.asksaveasfilename(defaultextension=".png")
-            if filepath:
-                self.image.save(filepath)
-
-    def resize_image(self):
-        if self.image:
-            new_size = tk.simpledialog.askstring("Resize", "Enter new size (width,height):")
-            if new_size:
-                width, height = map(int, new_size.split(','))
-                self.image = self.image.resize((width, height))
-                self.photo = ImageTk.PhotoImage(self.image)
-                self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-                self.root.geometry(f"{width}x{height}")
-
-# if __name__ == "__main__":
-    # root = tk.Tk()
-    # app = ImageEditor(root)
-    # root.mainloop()
+    app.mainloop()
