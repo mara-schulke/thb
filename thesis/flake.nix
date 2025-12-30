edbe595bbc7ebc6f1000c7d8edc0614b34881c2d
Mara Schulke <mail@maras.cloud>
2025-12-30 19:54:51 +0100

    Thesis flake

--- thesis/flake.nix
+++ thesis/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Thesis Environment";
+  description = "Applied Graph Kernels for Schema-Aware In-Context Learning in NL2SQL";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -20,12 +20,47 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        render = pkgs.writeShellApplication {
+          name = "render";
+          buildInputs = [
+            pkgs.pdflatex
+            pkgs.inkscape
+          ];
+          text = ''
+            ${pkgs.pdflatex}/bin/pdflatex --shell-escape "$@"
+          '';
+        };
+
+        thesis = pkgs.stdenv.mkDerivation {
+          name = "thesis";
+          src = ./.;
+
+          buildInputs = [ render ];
+
+          buildPhase = ''
+            rm -rf assets
+            mkdir -p assets/out
+            cp -r ${assets} assets/out
+            ${render}/bin/render
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp thesis.pdf $out
+          '';
+        };
       in
       {
+        packages = {
+          default = thesis;
+          thesis = thesis;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.inkscape
-            assets.packages.default
+            assets.packages.${system}.default
           ];
         };
       }
